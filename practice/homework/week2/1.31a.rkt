79ebe285123fac5bd31eb721a046762a9f6ac8f9
aliaksandr-s <s-a-n-9@mail.ru>
2018-06-09 23:57:08 +0300

    Add 1.31a

---
+++ practice/homework/week2/1.31a.rkt
@@ -0,0 +1,28 @@
+#lang racket/base
+
+(require rackunit)
+(require simply-scheme)
+
+(define (product term a next b)
+  (if (> a b)
+      1
+      (* (term a)
+         (product term (next a) next b))))
+
+(define (inc n) (+ n 1))
+
+(define (identity x) x)
+
+(define (fact n)
+  (product identity 1 inc n))
+
+(define (pi-term n) 
+  (if (even? n) 
+      (/ (+ n 2) (+ n 1)) 
+      (/ (+ n 1) (+ n 2))))
+
+(* (product pi-term 1 inc 6) 4)
+
+(check-equal?
+ (fact 5)
+ 120)
