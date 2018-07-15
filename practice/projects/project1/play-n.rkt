b285cbdc1b78f50ed1d66942eeb7653225e51f42
aliaksandr-s <s-a-n-9@mail.ru>
2018-07-15 20:33:36 +0300

    Add dealer-sensitive

--- practice/projects/project1/play-n.rkt
+++ practice/projects/project1/play-n.rkt
@@ -1,13 +1,23 @@
 #lang racket/base
 
 (require rackunit)
+(require racket/trace)
 (require simply-scheme)
 (require "./stop-at-17.rkt")
 (require "./twenty-one.rkt")
+(require "./dealer-sensitive.rkt")
 
 (define (play-n strategy n)
   (if (= n 0)
     0
     (+ (twenty-one strategy) (play-n strategy (- n 1)))))
 
-;;; (play-n stop-at-17 5)
+(play-n stop-at-17 500)
+(play-n dealer-sensitive 500)
+
+;;; (define (loop f n)
+;;;   (if (= n 0)
+;;;       `()
+;;;       (se (f) (loop f (- n 1)))))
+
+;;; (loop (play-n dealer-sensitive 100) 100)
