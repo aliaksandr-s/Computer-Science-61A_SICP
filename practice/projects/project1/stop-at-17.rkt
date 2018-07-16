92c79ead20ce0a6521fdc692f14ce7a086484add
aliaksandr-s <s-a-n-9@mail.ru>
2018-07-16 08:10:26 +0300

    Add stop-at

--- practice/projects/project1/stop-at-17.rkt
+++ practice/projects/project1/stop-at-17.rkt
@@ -3,13 +3,11 @@
 (require rackunit)
 (require simply-scheme)
 (require "./best-total.rkt")
+(require "./stop-at.rkt")
 
 (provide stop-at-17)
 
-(define (stop-at-17 customer-hand-so-far dealer-up-card)
-  (if (< (best-total customer-hand-so-far) 17) 
-    #t 
-    #f))
+(define stop-at-17 (stop-at 17))
 
 (check-equal? (stop-at-17 `(AD) `(4S)) #t)
 (check-equal? (stop-at-17 `(8S 8H) `(4S)) #t)
