bfef2aecd6f3496595e9e579ddb26bcc01a52785
aliaksandr-s <s-a-n-9@mail.ru>
2018-07-17 08:34:37 +0300

    Add suit strategy

--- practice/projects/project1/valentine.rkt
+++ practice/projects/project1/valentine.rkt
@@ -2,29 +2,12 @@
 
 (require rackunit)
 (require simply-scheme)
-(require "./best-total.rkt")
+(require "./suit-strategy.rkt")
+(require "./stop-at.rkt")
 
 (provide valentine)
 
-(define (has-heart? hand)
-  (cond 
-    [(empty? hand) #f]
-    [(equal? (last (first hand)) 'H) #t]
-    [else (has-heart? (butfirst hand))]))
-
-(check-equal? (has-heart? `(2S 2H)) #t)
-(check-equal? (has-heart? `(AH)) #t)
-
-(check-equal? (has-heart? `(2S AC 2D)) #f)
-(check-equal? (has-heart? `()) #f)
-
-(define (valentine customer-hand-so-far dealer-up-card)
-  (cond
-    [(< (best-total customer-hand-so-far) 17) #t]
-    [(and 
-      (has-heart? customer-hand-so-far) 
-      (< (best-total customer-hand-so-far) 19)) #t]
-    [else #f]))
+(define valentine (suit-strategy 'H (stop-at 17) (stop-at 19)))
 
 (check-equal? (valentine `(KD 6D) `(4S)) #t)
 (check-equal? (valentine `(KD 8H) `(4S)) #t)
