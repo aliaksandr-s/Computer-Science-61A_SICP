e2d54d475df6637e7ab4f95708dac506fcdf7b14
Alex S <s-a-n-9@mail.ru>
2018-09-09 19:13:21 +0300

    Add 2.36

---
+++ practice/homework/week_5/2.36.rkt
@@ -0,0 +1,19 @@
+#lang racket/base
+
+(require rackunit)
+(require racket/trace)
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (accumulate-n op init seqs)
+  (if (null? (car seqs))
+      '()
+      (cons (accumulate op init (map car seqs))
+            (accumulate-n op init (map cdr seqs)))))
+
+(define s '((1 2 3) (4 5 6) (7 8 9) (10 11 12)))
+(check-equal? (accumulate-n + 0 s) '(22 26 30))
