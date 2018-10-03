947fa16b16efc6abe7274aad50a69327284c0998
Alex S <s-a-n-9@mail.ru>
2018-10-03 12:41:23 +0300

    Add game helpers

---
+++ practice/projects/project_3/part-1.rkt
@@ -0,0 +1,48 @@
+#lang racket
+
+(require rackunit)
+(require simply-scheme)
+(require racket/trace)
+
+(require "./adv.rkt")
+(require "./tables.rkt")
+(require "./adv-world.rkt")
+
+;;; 1
+(define Dormitory (new place% [name 'Dormitory]))
+(define Kirin (new place% [name 'Kirin]))
+
+(can-go Soda 'north Kirin)
+(can-go Soda 'west Dormitory)
+(can-go Dormitory 'east Soda)
+(can-go Kirin 'south Soda)
+
+(define Alex (new person% [name 'Alex] [place Dormitory]))
+
+(define potstickers (new thing% [name 'potstickers]))
+(send Kirin appear potstickers)
+
+(send Dormitory exits)
+(send Alex go 'east)
+(send Alex go 'north)
+(send Alex look-around)
+(send Alex take potstickers)
+(send Alex go 'south)
+(send Alex go 'up)
+(send Alex go 'west)
+;;; (send Alex lose potstickers)
+(send Alex look-around)
+(send Brian take potstickers)
+
+
+;;; (define Joe (new person% [name 'Joe] [place Telegraph-Ave]))
+;;; (can-go Telegraph-Ave 'east (new place% [name 'Peoples-Park]))
+;;; (send Joe go 'east)
+;;; (send Joe get-place)
+
+;;; (let ((where (send Joe get-place)))
+;;;       (send where get-name))
+
+;;; (eq? (send Telegraph-Ave look-in 'east) (send Joe get-place))
+;;; (eq? (send Joe get-place) 'Peoples-Park)
+;;; (eq? (send (send Joe get-place) get-name) 'Peoples-Park)
