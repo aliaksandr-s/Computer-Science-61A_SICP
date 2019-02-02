a1c55b5e498c1ab90c37dd1fb550e9a2b339e129
Aliaksandr <s-a-n-9@mail.ru>
2019-02-02 15:11:01 +0400

    Add extend-environment

---
+++ practice/homework/week_12/extend-environment.rkt
@@ -0,0 +1,26 @@
+#lang racket
+
+(require rackunit)
+(require rnrs/mutable-pairs-6)
+(require compatibility/mlist)
+
+(define make-frame mcons)
+(define frame-variables mcar)
+(define frame-values mcdr)
+
+(define make-env mcons)
+(define first-frame mcar)
+(define enclosing-environment mcdr)
+(define the-empty-environment '())
+
+(define (extend-environment vars vals base-env)
+  (if (= (mlength vars) (mlength vals))
+    (make-env (make-frame vars vals) base-env)
+    (if (< (mlength vars) (mlength vals))
+      (error "Too many arguments supplied" vars vals)
+      (error "Too few arguments supplied" vars vals))))
+
+(define global-env (make-env (make-frame (mlist 'x) (mlist 3)) null))
+
+(check-equal? (extend-environment (mlist 'a 'b) (mlist 5 6) global-env) 
+              (make-env (make-frame (mlist 'a 'b) (mlist 5 6)) global-env))
